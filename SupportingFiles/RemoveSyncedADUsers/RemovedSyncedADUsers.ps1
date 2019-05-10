e27f2dd227c03fc545251efec63a117f9b2e591c
Steve Ross <steve@steverlabs.com>
2019-05-10 13:57:30 -0500

    Adding script to remove synce'ed AD accounts from AAD

---
+++ SupportingFiles/RemoveSyncedADUsers/RemovedSyncedADUsers.ps1
@@ -0,0 +1,13 @@
+﻿# Remove synchronized AD accounts from AzureAD
+# USE WITH CARE!
+Connect-AzureAD
+# Replace sprdemo.org below with your domain name
+$dom = '*sprdemo.org*'
+$acctList = Get-AzureADUser
+foreach ($i in $acctList) {
+    $upn = $i.UserPrincipalName
+    If ($upn -like $dom) 
+        {Write-Host "Deleting " + $i.UserPrincipalName 
+        Remove-AzureADUser -ObjectId $i.ObjectId
+        }
+    }
